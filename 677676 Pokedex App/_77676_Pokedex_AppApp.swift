5897ab3ea8d9ee614b1923e67e23ae83d5cb6d7f
RobyParak <677676@student.inholland.nl>
2024-10-07 08:51:10 +0100

    Initial Commit

---
+++ 677676 Pokedex App/_77676_Pokedex_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _77676_Pokedex_AppApp.swift
+//  677676 Pokedex App
+//
+//  Created by admin on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct _77676_Pokedex_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
