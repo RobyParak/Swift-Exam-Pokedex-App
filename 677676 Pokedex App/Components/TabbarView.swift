cc71c375a31e12d2bd6b6932bb9fff109cecf777
Roberta Paracchini <90033360+RobyParak@users.noreply.github.com>
2025-03-26 22:48:23 +0100

    Fav page sorted AND navigation is now swipeable

--- 677676 Pokedex App/Components/TabbarView.swift
+++ 677676 Pokedex App/Components/TabbarView.swift
@@ -6,16 +6,39 @@
 import SwiftUI
 
 struct TabbarView: View {
+    @State private var selectedTab = 0
+    
     var body: some View {
-        TabView {
-            MainPokemonPage(vm: MainPokemonPageViewModel(pokemonStore: PokemonStore()))
-                .tabItem {
-                    Label("Home", systemImage: "house")
-                }
-            FavouritePokemonPage()
-                .tabItem {
-                    Label("Favourites", systemImage: "heart")
-                }
+        NavigationView {
+            TabView(selection: $selectedTab) {
+                MainPokemonPage(vm: MainPokemonPageViewModel(pokemonStore: PokemonStore()))
+                    .gesture(
+                        DragGesture()
+                            .onEnded { value in
+                                if value.translation.width < -100 {
+                                    selectedTab = 1
+                                }
+                            }
+                    )
+                    .tabItem {
+                        Label("Home", systemImage: "house")
+                    }
+                    .tag(0)
+                
+                FavouritePokemonPage()
+                    .gesture(
+                        DragGesture()
+                            .onEnded { value in
+                                if value.translation.width > 100 {
+                                    selectedTab = 0
+                                }
+                            }
+                    )
+                    .tabItem {
+                        Label("Favorites", systemImage: "heart")
+                    }
+                    .tag(1)
+            }
         }
     }
 }
